e8d94a8754c679b356cb26695869fe230d0be017
abhiiishek-11 <abhishek20022004@gmail.com>
2026-06-02 10:26:32 +0530

    updated

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -5,7 +5,7 @@ export default function ComingSoon() {
 
   return (
 
-    <div className="min-h-screen bg-gradient-to-b from-[#021B2E] via-[#02263D] to-[#011826] text-white flex items-center justify-center px-6 relative overflow-hidden">
+    <div className="min-h-screen pt-[90px] md:pt-[120px] bg-gradient-to-b from-[#021B2E] via-[#02263D] to-[#011826] text-white flex items-center justify-center px-6 relative overflow-hidden ">
 
       {/* GLOW EFFECTS */}
 
@@ -15,13 +15,15 @@ export default function ComingSoon() {
 
       {/* CONTENT */}
 
-      <div className="relative z-10 text-center max-w-4xl">
+      <div className="relative z-10 text-center max-w-4xl mb-20">
 
         <p className="uppercase tracking-[0.35em] text-[#FFB703] text-sm font-semibold">
           Zyveniq Experience
         </p>
 
-        <h1 className="mt-8 text-5xl md:text-7xl font-black leading-[1.05]">
+        <h1 className="mt-8 text-4xl
+sm:text-5xl
+md:text-7xl font-black leading-[1.05]">
 
           Pardon Our Dust!
           <br />
@@ -40,7 +42,17 @@ export default function ComingSoon() {
 
           <br /><br />
 
-<h2 className="text-white/100 text-3xl md:text-3xl lg:text-7xl font-black tracking-[0.45em] uppercase mb-8">
+<h2 className="
+mt-12
+text-white
+text-3xl
+md:text-3xl
+lg:text-7xl
+font-black
+tracking-[0.45em]
+uppercase
+mb-8
+">
 
   Coming Soon
 
@@ -50,18 +62,28 @@ export default function ComingSoon() {
 
         {/* BUTTONS */}
 
-        <div className="mt-14 flex flex-wrap justify-center gap-5">
+        <div
+className="
+mt-14
+flex
+flex-col
+sm:flex-row
+justify-center
+items-center
+gap-5
+"
+>
 
           <Link
             to="/"
-            className="px-10 py-5 rounded-full bg-[#FFB703] text-[#023047] font-black hover:scale-105 transition-all duration-300 shadow-[0_10px_40px_rgba(255,183,3,0.35)]"
+            className="w-[280px] sm:w-auto px-10 py-5 rounded-full bg-[#FFB703] text-[#023047] font-black hover:scale-105 transition-all duration-300 shadow-[0_10px_40px_rgba(255,183,3,0.35)]"
           >
             Back To Home
           </Link>
 
           <Link
             to="/contact"
-            className="px-10 py-5 rounded-full border border-white/20 bg-white/5 backdrop-blur-xl text-white font-bold hover:bg-white/10 transition-all duration-300"
+            className="w-[280px] sm:w-auto px-10 py-5 rounded-full border border-white/20 bg-white/5 backdrop-blur-xl text-white font-bold hover:bg-white/10 transition-all duration-300"
           >
             Contact Us
           </Link>
